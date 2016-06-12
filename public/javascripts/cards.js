d3ac3764adc85b3071d9ec47444c5ce62f981f75
Sumit Gupta <sgupta@gwynniebee.com>
2016-06-12 17:38:36 +0530

    Final code added

--- public/javascripts/cards.js
+++ public/javascripts/cards.js
@@ -6,6 +6,7 @@ var Observables = {
 		var subscribers = this.topics.add,
 			hbObject = {},
 			i;
+		
 		//push the user data to array of cards
 		this.cards.push(userData);
 
@@ -28,38 +29,124 @@ var Observables = {
 
 	//function to sort the existing cards by name 
 	sortByName : function(){
+		var subscribers = this.topics.sort,
+			hbObjectsArr = [],
+			i,
+			hbObject={};
 
+		this.cards.sort(function(obj1, obj2){
+			return obj1.name>obj2.name ? 1 : -1;
+		});
+
+		for(i=0; i<this.cards.length; i++){
+			hbObject = this.createHbObjectForHandlebar(this.cards[i]);
+			hbObjectsArr.push(hbObject);
+		}
+
+		//for the add topic notify all the watchers
+		for(i=0; i<subscribers.length; i++){
+			subscribers[i](hbObjectsArr);
+		}
 	},
 	//function to sort the existing cards by location
 	sortByLocation : function(){
+		var subscribers = this.topics.sort,
+			hbObjectsArr = [],
+			i,
+			hbObject={};
 
+		this.cards.sort(function(obj1, obj2){
+			return obj1.location>obj2.location ? 1 : -1;
+		});
+
+		for(i=0; i<this.cards.length; i++){
+			hbObject = this.createHbObjectForHandlebar(this.cards[i]);
+			hbObjectsArr.push(hbObject);
+		}
+
+		//for the add topic notify all the watchers
+		for(i=0; i<subscribers.length; i++){
+			subscribers[i](hbObjectsArr);
+		}
 	},
+
 	//function to sort the existing cards by followers
 	sortByFollowers : function(){
+		var subscribers = this.topics.sort,
+			hbObjectsArr = [],
+			i,
+			hbObject={};
+
+		this.cards.sort(function(obj1, obj2){
+			return obj1.followers>obj2.followers ? 1 : -1;
+		});
+
+		for(i=0; i<this.cards.length; i++){
+			hbObject = this.createHbObjectForHandlebar(this.cards[i]);
+			hbObjectsArr.push(hbObject);
+		}
+
+		//for the add topic notify all the watchers
+		for(i=0; i<subscribers.length; i++){
+			subscribers[i](hbObjectsArr);
+		}
+	},
+	deleteCard : function(event){
+		var elem = event.currentTarget,
+			loginId = $(elem).data('id'),
+			subscribers = this.topics.delete,
+			index = null,
+			i;
+		
+		this.cards.forEach(function(userData, pos){
+			if(loginId === userData.login){
+				index = pos;
+			}
+		});
+
+		if(index !== null){
+			this.cards.splice(index, 1);
+			//for the add topic notify all the watchers
+			for(i=0; i<subscribers.length; i++){
+				subscribers[i](loginId);
+			}
+		}
 
 	},
-	deleteCard : function(){
 
-	},
 	createHbObjectForHandlebar : function(userData){
 		var hbObject = {
 				'name' : userData.name,
 				'location' : userData.location,
 				'followers' : userData.followers,
-				'imgSrc' : userData.avatar_url
+				'imgSrc' : userData.avatar_url,
+				'login' : userData.login,
+				'href' : userData.url
 			};
 		return hbObject;
+	},
+
+	_checkIfUserIsAlreadyNotPresent : function(loginId){
+		return this.cards.some(function(userData){
+			return loginId===userData.login;	
+		});
 	}
 };
 
 function addGitHubIdAsCard(event){
-	debugger;
 	var elem = event.currentTarget,
 		inputBoxId = $(elem).data('id'),
 		inputBox = $(inputBoxId),
 		inputBoxValue = $(inputBox).val(),
 		userData = {},
 		self = this;
+	if(Observables._checkIfUserIsAlreadyNotPresent(inputBoxValue)){
+		$('#duplicate-id').css('display', 'block');
+		return;
+	}
+	//set the display of loading to block to indicate the user
+	//data is being fetched
+	$('#loading').css('display', 'block');
 	//send ajax request to fetch user data
 	$.ajax({
         cache : false,
@@ -69,45 +156,60 @@ function addGitHubIdAsCard(event){
         contentType : 'application/json',
         timeout : 20000,
         success : function(resp){
-        	debugger;
         	userData = resp;
         	Observables.addCard(userData);
+        	$('#loading').css('display', 'none');
+        	//on success clear the login button
+        	$('#github-login').val('');
         }.bind(this),
         error : function(){
-        	//code for handling error cases
+        	$('#loading').css('display', 'none');
+        	//on error also clear the login button
+        	$('#github-login').val('');
+        	//display a generic error method if error occurs
+        	$('#geenric-error').css('display', 'block');
         }
     });
 }
 
 
 Observables.addSubscriber('add', function(hbObject){
-	debugger;
 	var cardTemplate = Handlebars.templates['card'],
 		htmlString = cardTemplate(hbObject);
 	$('#user-cards').append(htmlString);
 });	
 
+Observables.addSubscriber('delete', function(loginId){
+	$('#user-'+loginId).remove();
+});
+
+Observables.addSubscriber('sort', function(hbObjectsArr){
+	debugger;
+	var cardTemplate = Handlebars.templates['card'],
+		htmlString="";
+	hbObjectsArr.forEach(function(obj){
+		htmlString+=cardTemplate(obj);
+	});
+	//adding html string to user cards element
+	$('#user-cards').html(htmlString);
+});	
 
 
-function createHandlebarData(){
-
-}
-
-function sortCardsByName(){
-
-}
-
-function sortCardsByLocation(){
-
-}
-
-function sortCardsByFollowers(){
-
-}
 
 
-function deleteCards(){
 
-}
+$('#github-login').on('focus', function(){
+	//code to remove error as soon as user starts typing in input box
+	$('#geenric-error').css('display', 'none');
+	$('#duplicate-id').css('display', 'none');
+});
 
+//bind function for sorting functionality
+$('body').delegate('#sort-by-name', 'click', Observables.sortByName.bind(Observables));
+$('body').delegate('#sort-by-followers', 'click', Observables.sortByFollowers.bind(Observables));
+$('body').delegate('#sort-by-location', 'click', Observables.sortByLocation.bind(Observables));
+
+//on click of close icon delete the card
+$('body').delegate('.close-icon', 'click', Observables.deleteCard.bind(Observables));
+//on click of button functionality
 $('#github-login-button').on('click', this.addGitHubIdAsCard);
--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -3,7 +3,9 @@
 templates['card'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<li>\n	<div class=\"card-wrap\">\n		<div class=\"img-wrap\">\n			<img src=\""
+  return "<li id=\"user-"
+    + alias4(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"login","hash":{},"data":data}) : helper)))
+    + "\">\n	<div class=\"card-wrap\">\n		<div class=\"img-wrap\">\n			<img src=\""
     + alias4(((helper = (helper = helpers.imgSrc || (depth0 != null ? depth0.imgSrc : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imgSrc","hash":{},"data":data}) : helper)))
     + "\" alt=\"\" />\n		</div>\n		<div class=\"detail-wrap\">\n			<div class=\"name ellipsis\"><a>"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
@@ -13,6 +15,8 @@ templates['card'] = template({"compiler":[7,">= 4.0.0"],"main":function(containe
     + alias4(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"location","hash":{},"data":data}) : helper)))
     + "</span></a></div>\n			<div class=\"sub-head ellipsis\"><a><span>Followers:</span>&nbsp;"
     + alias4(((helper = (helper = helpers.followers || (depth0 != null ? depth0.followers : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"followers","hash":{},"data":data}) : helper)))
-    + "</a></div>\n		</div>\n		<span class=\"close-icon\">x</span>\n	</div>\n</li>";
+    + "</a></div>\n		</div>\n		<span class=\"close-icon\" data-id=\""
+    + alias4(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"login","hash":{},"data":data}) : helper)))
+    + "\">x</span>\n	</div>\n</li>";
 },"useData":true});
 })();
